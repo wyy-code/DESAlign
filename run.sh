6ff41323a0ede787b016c8f50a0d9b52291d7fb3
YuanYi <54620490+wyy-code@users.noreply.github.com>
2023-11-19 13:45:17 +0800

    Add files via upload

---
+++ run.sh
@@ -0,0 +1,48 @@
+# Command Details:
+# bash file / GPU / Dataset / data split / R_{seed} / R_{img}
+# Begin Main Results:
+# ---------- Normal & R_{img} = 1 & iter. DESAlign ----------
+bash run_desalign_00.sh 1 DBP15K fr_en 0.3 1.0
+bash run_desalign_00.sh 1 DBP15K ja_en 0.3 1.0
+bash run_desalign_00.sh 1 DBP15K zh_en 0.3 1.0
+# Begin Img Ratio:
+# ---------- R_{img} = 0.05 & non-iter. DESAlign ----------
+bash run_desalign_0.sh 1 DBP15K fr_en 0.3 0.05
+bash run_desalign_0.sh 1 DBP15K ja_en 0.3 0.05
+bash run_desalign_0.sh 1 DBP15K zh_en 0.3 0.05
+# ---------- R_{img} = 0.2 & non-iter. DESAlign ----------
+bash run_desalign_0.sh 1 DBP15K fr_en 0.3 0.2
+bash run_desalign_0.sh 1 DBP15K ja_en 0.3 0.2
+bash run_desalign_0.sh 1 DBP15K zh_en 0.3 0.2
+# ---------- R_{img} = 0.4 & non-iter. DESAlign ----------
+bash run_desalign_0.sh 1 DBP15K fr_en 0.3 0.4
+bash run_desalign_0.sh 1 DBP15K ja_en 0.3 0.4
+bash run_desalign_0.sh 1 DBP15K zh_en 0.3 0.4
+# ---------- R_{img} = 0.6 & non-iter. DESAlign ----------
+bash run_desalign_0.sh 1 DBP15K fr_en 0.3 0.6
+bash run_desalign_0.sh 1 DBP15K ja_en 0.3 0.6
+bash run_desalign_0.sh 1 DBP15K zh_en 0.3 0.6
+# ---------- R_{img} = 0.1 & non-iter. DESAlign ----------
+# bash run_desalign_0.sh 1 DBP15K fr_en 0.3 0.1
+# bash run_desalign_0.sh 1 DBP15K ja_en 0.3 0.1
+# bash run_desalign_0.sh 1 DBP15K zh_en 0.3 0.1
+# ---------- R_{img} = 0.15 & non-iter. DESAlign ----------
+# bash run_desalign_0.sh 1 DBP15K fr_en 0.3 0.15
+# bash run_desalign_0.sh 1 DBP15K ja_en 0.3 0.15
+# bash run_desalign_0.sh 1 DBP15K zh_en 0.3 0.15
+# ---------- R_{img} = 0.3 & non-iter. DESAlign ----------
+bash run_desalign_0.sh 1 DBP15K fr_en 0.3 0.3
+bash run_desalign_0.sh 1 DBP15K ja_en 0.3 0.3
+bash run_desalign_0.sh 1 DBP15K zh_en 0.3 0.3
+# ---------- R_{img} = 0.45 & non-iter. DESAlign ----------
+# bash run_desalign_0.sh 1 DBP15K fr_en 0.3 0.45
+# bash run_desalign_0.sh 1 DBP15K ja_en 0.3 0.45
+# bash run_desalign_0.sh 1 DBP15K zh_en 0.3 0.45
+# ---------- R_{img} = 0.5 & non-iter. DESAlign ----------
+bash run_desalign_0.sh 1 DBP15K fr_en 0.3 0.5
+bash run_desalign_0.sh 1 DBP15K ja_en 0.3 0.5
+bash run_desalign_0.sh 1 DBP15K zh_en 0.3 0.5
+# ---------- R_{img} = 0.55 & non-iter. DESAlign ----------
+# bash run_desalign_0.sh 1 DBP15K fr_en 0.3 0.55
+# bash run_desalign_0.sh 1 DBP15K ja_en 0.3 0.55
+# bash run_desalign_0.sh 1 DBP15K zh_en 0.3 0.55
